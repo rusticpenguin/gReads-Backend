a35e0ba020df171848ba79d15df760cfddf920e3
rusticpenguin <georgechios.music@gmail.com>
2018-09-02 18:24:49 -0600

    Added delete route

--- app.js
+++ app.js
@@ -6,28 +6,14 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 
+const Book = require('./models/book');
+const Author = require('./models/author');
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 
 mongoose.connect(process.env.MONGODBURL, { useNewUrlParser: true });
-
-const bookSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    bookGenre: { type: String, required: false },
-    bookDescription: { type: String, required: false},
-    bookCoverURL: { type: String, required: false},
-});
-
-const authorSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: {type: String, required: true },
-    bio: { type: String, required: false},
-    portraitURL: { type: String, required: false}
-});
-
-const Book = mongoose.model('Book', bookSchema);
-const Author = mongoose.model('Author', authorSchema);
 
 app.get("/", (req, res) => {
     res.redirect("/");
@@ -36,32 +22,41 @@ app.get("/", (req, res) => {
 app.route("/books")
     .get((req, res) => {
         Book.find({}, (err, foundBooks) => {
-            if (err){
-                res.status(404).end();
-            } else {
-                res.status(200).json({foundBooks});
+            if (err) res.status(404).end();
+            res.status(200).json({foundBooks});
             }
-        });
+        );
     })
     .post((req, res) => {
         Book.create(req.body, (err)=>{
-            if(err){
-                res.status(418).end();
+            if(err) res.status(418).end();
+            res.status(200).json(req.body).end();
             }
-        })
+        )
     })
 
 app.route("/books/:id")
     .get((req, res) => {
         Book.findById(req.params.id, (err, foundBooks) => {
-            if (err){
-                res.status(404).end();
-            } else {
-                res.status(200).json({foundBooks});
+            if (err) res.status(404).end();
+            res.status(200).json({foundBooks});
             }
+        );
+    })
+    .put((req, res) => {
+        Book.findByIdAndUpdate({_id: req.params.id})
+            .then(data => {
+                Book.findOne({_id: req.params.id})
+                    .then(data => res.send(data))
+            });
+    })
+    .delete((req, res) => {
+        Book.deleteOne({_id: req.params.id}, (err, book) => {
+            if (err) return res.send(err);
+            res.json({ message: 'Book has been Deleted'});
         });
     })
 
 app.listen(port, () => {
-    console.log("listening on port ", port)
+    console.log("listening on port ", port);
 })
